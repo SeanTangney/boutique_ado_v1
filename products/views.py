5d0bc5d986300fed5486f6ee2c0c0c18651014c4
SeanTangney <sean_tangney@hotmail.com>
2022-07-02 18:30:32 +0000

    Added shopping bag app, urls, template

--- products/views.py
+++ products/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
 
 
 def all_products(request):
-
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
@@ -25,7 +26,6 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -40,7 +40,7 @@ def all_products(request):
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "You didnt enter any search criteria!")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(description__icontains=query)
@@ -59,7 +59,6 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-
     """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
