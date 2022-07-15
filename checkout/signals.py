a7410ec7a8e551725da90408a6679442e2e69961
SeanTangney <sean_tangney@hotmail.com>
2022-07-15 16:33:01 +0000

    Added forms

---
+++ checkout/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispach import reciever
+
+from .models import OrderLineItem
+
+@reciever(post, save, sender=OrderItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    update order total on lineitem update/create
+    """
+    instance.order.update_total()
